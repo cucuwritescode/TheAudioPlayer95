930e24a13036e47b84c61530fbba5e0bcd2ff0e9
cucuwritescode <anicsounds@gmail.com>
2024-07-14 01:35:00 -0300

    file opening and some of the probing to find out the audio file format

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 //arrancamos importando los modulos que voy a necesitar 
 //del crate de symophonia para manejar archivos wav
-
 use symphonia::core::codecs::DecoderOptions;
+use symphonia::core::formats::FormatOptions;
+use symphonia::core::io::MediaSourceStream;
 use symphonia::core::FormatOptions;
 use symphonia::io::MediaSourceStream;
 use symphonia::core::meta::MetadataOptions;
 use symphonia::core::probe::Hint;
 use symphonia::default::get_probe;
+//modulos necesarios de rodio para el playback de audio
+use rodio::{Decoder as RodioDecoder, OutputStream, Source}; 
 
 //estas proximas dos son librerias estandar
 use std::io::BufReader;
 use std::fs::File;
+use std::time::Duration;
 
+fn main() {
+    let file = File::open("audio.wav").expect("Failed to open file");
+    let buf_reader= BufReader::new(File);
+    let media_source_stream= MediaSourceStream::new(Box::new(buf_reader)),Default::default();
+    let hint= Hint::new();
+    let probed= get_probe().format(
+        &hint,
+        media_source_stream,
+        &FormatOptions::default(),
+        &MetadataOptions::default(),
+    ).expect("Failed to probe format");
+    
+
+    
+}
 
 
 
